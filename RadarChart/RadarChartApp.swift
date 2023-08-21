d4fda59449c2394f186f80d007d50d8cd9e735f6
AIWASS23 <marcelodearaujo677@gmail.com>
2023-08-21 08:56:10 -0300

    Initial Commit

---
+++ RadarChart/RadarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadarChartApp.swift
+//  RadarChart
+//
+//  Created by Marcelo De Araújo on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct RadarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
